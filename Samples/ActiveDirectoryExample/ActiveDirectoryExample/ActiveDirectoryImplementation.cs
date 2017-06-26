c28a46d4efc1cae438a73e32c2350c7b62f2811c
Casper Jensen <c@sper.name>
2017-06-26 12:19:04 +0200

    initial commit of AD Sample

---
+++ Samples/ActiveDirectoryExample/ActiveDirectoryExample/ActiveDirectoryImplementation.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using System.DirectoryServices;
+
+namespace ActiveDirectoryExample
+{
+    public class ActiveDirectoryImplementation : IHRSystem
+    {
+        //TODO: Inset you own Path
+        private const string LDAP_SEARCH_PATH = "LDAP://OU=Users,OU=Howdy,DC=wlb,DC=local";
+        public JArray GetAllHowdyUsers()
+        {
+            DirectoryEntry ldapConnection = CreateLdapConnection();
+
+            DirectorySearcher search = new DirectorySearcher(ldapConnection);
+            search.Filter = "(objectclass=person)";
+
+
+            var allEmployees = search.FindAll();
+
+            if (allEmployees != null && allEmployees.Count > 0)
+            {
+                var howdyPayload = ExtractHowdyData(allEmployees);
+
+                return howdyPayload;
+            }
+            else
+            {
+                return null;
+            }
+
+
+        }
+
+
+        DirectoryEntry CreateLdapConnection()
+        {
+            var ldapConnection = new DirectoryEntry();
+            ldapConnection.Path = LDAP_SEARCH_PATH;
+            ldapConnection.AuthenticationType = AuthenticationTypes.Secure;
+
+            return ldapConnection;
+        }
+
+
+        private JArray ExtractHowdyData(SearchResultCollection allEmployees)
+        {
+            return new JArray(from SearchResult employee in allEmployees
+                              select new JObject(
+                                  //INFO: Use a unique ID that never changes. This is used to uniquely identify an employee. userPrincipalName could alo be used
+                                  new JProperty("EmployeeID", GetPropertyValue(employee, "objectGUID")),
+                                  new JProperty("Firstname", GetPropertyValue(employee, "givenName")),
+                                  new JProperty("Lastname", GetPropertyValue(employee, "sn")),
+                                  new JProperty("Email", GetPropertyValue(employee, "mail")),
+                                  new JProperty("Phonenumber", GetPropertyValue(employee, "telephoneNumber")),
+                                  new JProperty("JobTitle", GetPropertyValue(employee, "title")),
+                                  new JProperty("Department", GetPropertyValue(employee, "department"))
+                                  //INFO: Add more properties here
+                                  //new JProperty("", GetPropertyValue(employee, ""))
+
+
+                                  ));
+
+
+        }
+
+        private object GetPropertyValue(SearchResult employee, string property)
+        {
+            if (employee.Properties.Contains(property) && employee.Properties[property].Count > 0)
+            {
+                var rawData = employee.Properties[property][0];
+                if (rawData == null) return null;
+
+                if (rawData is byte[])
+                {
+                    return new Guid((byte[])rawData).ToString("N");
+                }
+                else
+                {
+                    return rawData;
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ Samples/ActiveDirectoryExample/ActiveDirectoryExample/Program.cs
@@ -0,0 +1,65 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.DirectoryServices;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActiveDirectoryExample
+{
+    class Program
+    {
+        private const string HOWDY_API_URL = "https://wlb-uat-ne1-api.azurewebsites.net/";
+        //INFO: Use this const for production
+        //private const string HOWDY_API_URL = "https://api-ne1.worklifebarometer.com/";
+
+        //TODO: Insert your JWT token here. Can be obtained from the portal
+        private const string HOWDY_API_TOKEN = "";
+        //TODO: Insert your Howdy Company ID here
+        private const int HOWDY_COMPANY_ID = 100000;
+
+        static void Main(string[] args)
+        {
+            Task.Run(StartAsync).GetAwaiter().GetResult();
+        }
+
+
+        private static async Task StartAsync()
+        {
+            IHRSystem hrSystem = new ActiveDirectoryImplementation();
+            var employeeData = hrSystem.GetAllHowdyUsers();
+
+            Console.WriteLine("Howdy Data to be posted:");
+            Console.WriteLine(employeeData);
+
+            Console.WriteLine("Posting data");
+            await SendDataToHowdyAsync(employeeData);
+
+            Console.WriteLine("Done. Press [Enter] to exit");
+            Console.ReadLine();
+        }
+
+        private static async Task SendDataToHowdyAsync(JToken howdyPayload)
+        {
+
+
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", HOWDY_API_TOKEN);
+            httpClient.BaseAddress = new Uri(HOWDY_API_URL);
+            var response = await httpClient.PutAsync($"/v1.0/Company/{HOWDY_COMPANY_ID}/Employee", 
+                new StringContent(howdyPayload.ToString(), Encoding.UTF8, "application/json"));
+
+
+            Console.WriteLine("Response from Howdy");
+            Console.WriteLine(await response.Content.ReadAsStringAsync());
+
+        }
+
+        
+
+        
+    }
+}
